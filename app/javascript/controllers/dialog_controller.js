93852e0e0a18663aa19d8129fa6240d9c860699e
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-06-19 19:05:43 -0300

    misc fixes

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -3,17 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="dialog"
 export default class Dialog extends Controller {
   static targets = ["dialog"]
-  connect() {
-    console.log("dialog connected", this.dialogTarget)
-  }
-
   open() {
-    console.log("open clicked")
     this.dialogTarget.show()
   }
 
   close() {
-    console.log("close clicked")
     this.dialogTarget.hide()
   }
 }
