7000f3e3c7ddadda78ea4c8299f24c20fd37d0c3
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-06-21 19:38:08 -0300

    reset add task form on submit

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,13 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class Dropdown extends Controller {
-  static targets = ["dropdown"]
 
-  connect() {
-    console.log(this.element)
-  }
   hide() {
-    console.log("hide")
     this.element.hide()
   }
 }
