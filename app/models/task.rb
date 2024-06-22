aceff97b02e1c652533b3750297394ae07fa95af
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-06-22 17:47:11 -0300

    Add task model validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  belongs_to :project
+  belongs_to :project, dependent: :destroy
   has_many :users, through: :project
   acts_as_list scope: :project
 
   validates :title, presence: true, length: { maximum: 100 }
+  validates :completed, inclusion: { in: [true, false] }
 end
