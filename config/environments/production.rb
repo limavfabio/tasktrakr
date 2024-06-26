9bc0b25b9faa75835973a96c887722cb96d7f871
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-06-26 20:45:15 -0300

    Disable mail delivery in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Disable mail delivery in production.
+  config.action_mailer.perform_deliveries = false
 end
