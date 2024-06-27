b0e2ee244c06d903018517cc54595bec6b3c3f12
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-06-26 21:27:49 -0300

    Fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-        #  :confirmable
+         :recoverable, :rememberable, :validatable
+  #  :confirmable
 
   has_many :user_projects, dependent: :destroy
   has_many :projects, through: :user_projects
