d4e9bd6dafea4f2d17cef2e2a767aa994ceae3fd
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-06-26 21:26:47 -0300

    Disable devise confirmable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+        #  :confirmable
 
   has_many :user_projects, dependent: :destroy
   has_many :projects, through: :user_projects
